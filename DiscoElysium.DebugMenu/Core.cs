68c1861e0922931d974238338846e65f47503f3c
kolbasa kopchennaya <no email>
2026-06-27 10:51:53 +0300

    Добавьте файлы проекта.

---
+++ DiscoElysium.DebugMenu/Core.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Il2CppSunshine;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(DiscoElysium.DebugMenu.Core), "DiscoElysium.DebugMenu", "1.0.0", "kolbasa99", null)]
+[assembly: MelonGame("ZAUM Studio", "Disco Elysium")]
+
+namespace DiscoElysium.DebugMenu
+{
+    public class Core : MelonMod
+    {
+    }
+
+    [HarmonyPatch(typeof(DebugModes), nameof(DebugModes.Awake))]
+    internal static class DebugModes_Awake_Patch
+    {
+        private static void Postfix()
+        {
+            DebugModes.CurrentMode = DebugMode.DemoMode;
+        }
+    }
+}
